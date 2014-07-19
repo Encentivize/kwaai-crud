1d990705ed3f8adcf1dc5ee8ae9f9ac3a8ef9056
unknown <martin@MartinN-PC.entelect.local>
2014-07-19 17:19:31 +0200

    Fixesfor options not allowing false values. Addition of countByQuery

--- testing.js
+++ testing.js
@@ -1,1 +1,15 @@
 var kwaaiCrud=require('./lib/crud.js');
+
+
+function getOptionDefaults(options)
+{
+    if (typeof options.validate == "undefined" || options.validate == null){options.validate=true}
+    if (typeof options.coerce == "undefined" || options.coerce == null){options.coerce=false}
+}
+
+var options={
+
+}
+
+getOptionDefaults(options)
+console.log("here")
